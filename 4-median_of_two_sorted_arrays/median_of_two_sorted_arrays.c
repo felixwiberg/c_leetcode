53e003de344c29f1e5057633042723fec7783aef
felixwiberg <felixwiberg@gmail.com>
2019-03-02 15:36:21 +0100

    4

--- 4-median_of_two_sorted_arrays/median_of_two_sorted_arrays.c
+++ 4-median_of_two_sorted_arrays/median_of_two_sorted_arrays.c
@@ -2,12 +2,18 @@
 
 double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size) {
     int midpoint = (nums1Size + nums2Size)/2;
-    int odd = (nums1Size + nums2Size)%2;
-    
+    int odd = (nums1Size + nums2Size)%2; 
     int i1 = 0, 
         i2 = 0,
         current = 0,
         prev = 0;
+
+    if(nums1Size == 0){
+            return odd?nums2[midpoint]:(nums2[midpoint]+nums2[midpoint-1])/2.0;
+    }
+    if(nums2Size == 0){
+            return odd?nums1[midpoint]:(nums1[midpoint]+nums1[midpoint-1])/2.0;
+    }
 
     int c1 = nums1[0],
         c2 = nums2[0];
@@ -16,14 +22,14 @@ double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Si
         prev = current;
         if(c1<c2){
             current = c1;
-            if(i1 + 1==nums1Size){
+            if(i1>nums1Size-1){
                 c1 = 10000;
             }else{
                 c1 = nums1[++i1];
             }
         }else{
             current = c2;
-            if(i2 + 1==nums2Size){
+            if(i2>nums2Size-1){
                 c2 = 10000;
             }else{
                 c2 = nums2[++i2];
@@ -35,9 +41,9 @@ double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Si
 }
 
 int main(){
-    int nums1[] = {1,3};
-    int nums2[] = {1000};
-    double res = findMedianSortedArrays(nums1, 2, nums2, 1);
+    int nums1[] = {};
+    int nums2[] = {1,2,2};
+    double res = findMedianSortedArrays(nums1, 0, nums2, 3);
     printf("%5.3f", res);
     return 0;
 }
