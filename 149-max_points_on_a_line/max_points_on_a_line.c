b0e54096ab8558232a6e80f7d84303107c199fea
felixwiberg <felixwiberg@gmail.com>
2019-03-02 17:42:19 +0100

    149

--- 149-max_points_on_a_line/max_points_on_a_line.c
+++ 149-max_points_on_a_line/max_points_on_a_line.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 struct Point {
     int x;
     int y;
@@ -25,9 +25,8 @@ int maxPoints(struct Point* points, int pointsSize) {
             if(xdiff != 0){
                 k = ydiff/xdiff;
                 m = points[i].y-k*points[i].x;
-
                 for(int l = 0; l<pointsSize; l++){
-                    if(points[l].y == k*points[l].x + m){
+                    if(abs(points[l].y - k*points[l].x + m) < 0.001){
                         this_line_count++;
                     }
                 }
@@ -46,16 +45,16 @@ int maxPoints(struct Point* points, int pointsSize) {
 
 int main(){
     struct Point a[4];
-    a[0].x = -1;
-    a[0].y = -1;
-    a[1].x = 0;
-    a[1].y = 0;
-    a[2].x = 1;
-    a[2].y = 6;
-    a[3].x = 2;
-    a[3].y = 2;
+    a[0].x = 3;
+    a[0].y = 1;
+    a[1].x = -6;
+    a[1].y = -1;
+    a[2].x = 3;
+    a[2].y = 1;
+    a[3].x = 12;
+    a[3].y = 3;
 
-    int res = maxPoints(a, 1);
+    int res = maxPoints(a, 4);
     printf("%d", res);
     return 0;
 }
