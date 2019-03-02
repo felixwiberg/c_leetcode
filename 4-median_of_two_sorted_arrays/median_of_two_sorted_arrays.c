d0af1a729aec2d14989f89d2646aeea3e2705fe1
felixwiberg <felixwiberg@gmail.com>
2019-03-02 15:26:07 +0100

    4

--- 4-median_of_two_sorted_arrays/median_of_two_sorted_arrays.c
+++ 4-median_of_two_sorted_arrays/median_of_two_sorted_arrays.c
@@ -4,35 +4,39 @@ double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Si
     int midpoint = (nums1Size + nums2Size)/2;
     int odd = (nums1Size + nums2Size)%2;
     
-    int i1 = 0, i2 = 0;
-    double res = 0;
-    int current = 0;
-    int prev = 0;
-    
+    int i1 = 0, 
+        i2 = 0,
+        current = 0,
+        prev = 0;
+
+    int c1 = nums1[0],
+        c2 = nums2[0];
+
     for(int i = 0; i <= midpoint; i++){
         prev = current;
-        if(i2==nums2Size || nums1[i1] < nums2[i2]){
-            current = nums1[i1];
-            i1++;
-        }else if(i1==nums1Size || nums2[i2] < nums1[i1]){
-            current = nums2[i2];
-            i2++;
+        if(c1<c2){
+            current = c1;
+            if(i1 + 1==nums1Size){
+                c1 = 10000;
+            }else{
+                c1 = nums1[++i1];
+            }
         }else{
+            current = c2;
+            if(i2 + 1==nums2Size){
+                c2 = 10000;
+            }else{
+                c2 = nums2[++i2];
+            }
         }
-        printf("%d ", prev);
-        printf("%d \n",current);
+        printf("%d %d\n", prev, current);
     }
-    if(odd){
-        res = current;
-    }else{
-        res = (current+prev)/2.0;
-    }
-    return res;
+    return odd?current:(current+prev)/2.0;
 }
 
 int main(){
     int nums1[] = {1,3};
-    int nums2[] = {2};
+    int nums2[] = {1000};
     double res = findMedianSortedArrays(nums1, 2, nums2, 1);
     printf("%5.3f", res);
     return 0;
