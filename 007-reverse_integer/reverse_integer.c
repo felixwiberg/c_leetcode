ece9b83f8acdf33174e47d6a0a94704e11e0f380
felixwiberg <felixwiberg@gmail.com>
2019-03-09 18:04:26 +0100

    007

--- 007-reverse_integer/reverse_integer.c
+++ 007-reverse_integer/reverse_integer.c
@@ -1,30 +1,30 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 int reverse(int x) {
     bool negative = x<0?true:false;
-    if(negative){
-        x *+ -1;
-    }
-    long tmp, ret = 0;
-    while(x>0){
+    int tmp = 0;
+    int ret = 0;
+    while(x!=0){
+        if (abs(ret) > 2147483648/10){
+            return 0;
+        }
         ret *= 10;
         tmp = x%10;
         ret += tmp;
         x /= 10;
     }
-    if (ret > 2147483648){
-        return 0;
-    }
     if(negative){
-        x *= -1;
+        ret *= -1;
     }
     return (int)ret;
 }
 
 int main(){
     //int ret = reverse(657);
-    int ret = reverse(1534236469);
+    //int ret = reverse(1534236469);
+    int ret = reverse(-2147483648);
     printf("%d", ret);
     return 0;
 }
