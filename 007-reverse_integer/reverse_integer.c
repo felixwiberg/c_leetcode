072eb74a13f08a3d1e4406aa83588f4a6c5d75ab
felixwiberg <felixwiberg@gmail.com>
2019-03-09 18:11:17 +0100

    007

--- 007-reverse_integer/reverse_integer.c
+++ 007-reverse_integer/reverse_integer.c
@@ -4,12 +4,9 @@
 #include <limits.h>
 
 int reverse(int x) {
-    int tmp = 0;
     int ret = 0;
     while(x!=0){
-        if (abs(ret) > INT_MAX/10){
-            return 0;
-        }
+        if (abs(ret) > INT_MAX/10){return 0;}
         ret = ret*10 + x%10;
         x /= 10;
         printf("%d\n",ret);
